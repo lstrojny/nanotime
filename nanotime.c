6dc66655aa3ef6096477e5098314c973c79f6654
Lars Strojny <lars@strojny.net>
2013-10-12 19:52:16 +0200

    Adding header file

--- nanotime.c
+++ nanotime.c
@@ -1,24 +1,12 @@
-#include <time.h>
+#include "nanotime.h"
 #include <stdio.h>
 
 #ifdef __MACH__
 #include <mach/mach.h>
 #include <mach/clock.h>
-#define NANO extern
 #else
 #include <sys/time.h>
-#define NANO
 #endif
-
-#define NANO_FAILURE -1
-#define NANO_SUCCESS 0
-#define NANO_EXPECTED(X) X == NANO_SUCCESS
-#define NANO_UNEXPECTED(X) X == NANO_FAILURE
-#define nano_return_t short
-
-NANO nano_return_t current_nano_second(unsigned long *second);
-NANO nano_return_t current_nano_time(long double *time);
-NANO nano_return_t nano_now(struct timespec *now);
 
 NANO nano_return_t nano_now(struct timespec *now)
 {
---
+++ nanotime.h
@@ -0,0 +1,16 @@
+#include <time.h>
+#ifdef __MACH__
+#define NANO extern
+#else
+#define NANO
+#endif
+
+#define NANO_FAILURE -1
+#define NANO_SUCCESS 0
+#define NANO_EXPECTED(X) X == NANO_SUCCESS
+#define NANO_UNEXPECTED(X) X == NANO_FAILURE
+#define nano_return_t short
+
+NANO nano_return_t current_nano_second(unsigned long *second);
+NANO nano_return_t current_nano_time(long double *time);
+NANO nano_return_t nano_now(struct timespec *now);
